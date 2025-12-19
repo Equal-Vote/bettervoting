7134b65d3766abdf3698e0a583ae702932a310cc
Arend Peter <apc1993@gmail.com>
2025-12-19 15:19:40 -0800

    Refactor election settings

--- packages/frontend/src/components/ElectionForm/ElectionSettings.tsx
+++ packages/frontend/src/components/ElectionForm/ElectionSettings.tsx
@@ -119,131 +119,72 @@ export default function ElectionSettings() {
         <FormHelperText hidden={!helperText} sx={{ mb:2, mt:0, lineHeight: 0, fontStyle: 'italic', textAlign: 'center' }}>{t(`disabled_msgs.${setting}`)}</FormHelperText>
     </>;
 
-    return (
-        <Paper elevation={3} sx={{width: '100%', px: 4, py: 1}}>
-            <Box
-                sx={{ display: 'flex', bgcolor: 'background.paper', borderRadius: 10 }}
-                alignItems={'center'}
-            >
-                <Box sx={{ width: '100%', pl: 2 }}>
-                    <Typography variant="h4" component="h4">{t('election_settings.button_title')}</Typography>
+    return <Grid item xs={12} sx={{ m: 0, my: 0, p: 1 }}>
+        <FormControl disabled={election.state !== 'draft'} component="fieldset" variant="standard">
+            <FormGroup>
+                <FormControlLabel control={
+                    <TextField
+                        id="contact_email"
+                        value={editedElectionSettings.contact_email ? editedElectionSettings.contact_email : ''}
+                        onChange={(e) => applySettingsUpdate((settings) => { settings.contact_email = e.target.value })}
+                        variant='standard'
+                        fullWidth
+                        sx={{ mt: -1, display: 'block'}}
+                    />}
+                    label={t('election_settings.contact_email')}
+                    labelPlacement='top'
+                    sx={{
+                        alignItems: 'start',
+                        mb: 3
+                    }}
+                />
+
+                <Box sx={{mt: 3, mb: 2}}>
+                    <FormLabel>
+                        {t('wizard.term_question')}
+                        <Tip name='polls_vs_elections'/>
+                    </FormLabel>
+                    <RadioGroup row>
+                        {['poll', 'election'].map( (type, i) => 
+                            <FormControlLabel
+                                key={i}
+                                control={<Radio
+                                    onChange={(() => {                                                
+                                        applySettingsUpdate(settings => settings.term_type = type as TermType )
+                                    })}
+                                    checked={editedElectionSettings.term_type === type}
+                                    value={t(`keyword.${type}.election`)}
+                                />}
+                                label={capitalize(t(`keyword.${type}.election`))}
+                            />
+                        )}
+                    </RadioGroup>
                 </Box>
-                <Box sx={{ flexShrink: 1, p: 1 }}>
-                    <IconButton
-                        aria-label="Edit Settings"
-                        onClick={handleOpen}>
-                        {election.state  === 'draft' ? <EditIcon /> : <VisibilityIcon />}
-                    </IconButton>
-                </Box>
-            </Box>
-            <Dialog
-                open={open}
-                onClose={handleClose}
-            >
-                <DialogTitle sx={{m: 0}}>{t('election_settings.dialog_title')}</DialogTitle>
-                <DialogContent >
-                    <Grid item xs={12} sx={{ m: 0, my: 0, p: 1 }}>
-                        <FormControl disabled={election.state !== 'draft'} component="fieldset" variant="standard">
-                            <FormGroup>
-                                <FormControlLabel control={
-                                    <TextField
-                                        id="contact_email"
-                                        value={editedElectionSettings.contact_email ? editedElectionSettings.contact_email : ''}
-                                        onChange={(e) => applySettingsUpdate((settings) => { settings.contact_email = e.target.value })}
-                                        variant='standard'
-                                        fullWidth
-                                        sx={{ mt: -1, display: 'block'}}
-                                    />}
-                                    label={t('election_settings.contact_email')}
-                                    labelPlacement='top'
-                                    sx={{
-                                        alignItems: 'start',
-                                        mb: 3
-                                    }}
-                                />
+                
 
-                                <Box sx={{mt: 3, mb: 2}}>
-                                    <FormLabel>
-                                        {t('wizard.term_question')}
-                                        <Tip name='polls_vs_elections'/>
-                                    </FormLabel>
-                                    <RadioGroup row>
-                                        {['poll', 'election'].map( (type, i) => 
-                                            <FormControlLabel
-                                                key={i}
-                                                control={<Radio
-                                                    onChange={(() => {                                                
-                                                        applySettingsUpdate(settings => settings.term_type = type as TermType )
-                                                    })}
-                                                    checked={editedElectionSettings.term_type === type}
-                                                    value={t(`keyword.${type}.election`)}
-                                                />}
-                                                label={capitalize(t(`keyword.${type}.election`))}
-                                            />
-                                        )}
-                                    </RadioGroup>
-                                </Box>
-                                
+                <CheckboxSetting setting='random_candidate_order' />
+                { ballotUpdatesConditionsMet && <CheckboxSetting setting='ballot_updates' hidden={!ballotUpdatesConditionsMet}
+                    disabled={election.state !== 'draft' || ballotUpdatesDisabled} checked={ballotUpdates} onChange={onChangeBallotUpdates} helperText={ballotUpdatesDisabledMsg}/>}
+                { ['draft', 'finalized', 'open'].includes(election.state) && <CheckboxSetting setting='public_results' checked={publicResults}
+                    onChange={onChangePublicResults} disabled={election.state !== 'draft' || publicResultsDisabled} helperText={publicResultsDisabled}/>}
+                <CheckboxSetting setting='require_instruction_confirmation'/>
+                <CheckboxSetting setting='draggable_ballot'/>
+                <CheckboxSetting setting='is_public'/>
+                <CheckboxSetting setting='max_rankings' onChange={(e) => applySettingsUpdate(settings => {
+                    settings.max_rankings = e.target.checked ? default_rankings : undefined })
+                }/>
 
-                                <CheckboxSetting setting='random_candidate_order' />
-                                { ballotUpdatesConditionsMet && <CheckboxSetting setting='ballot_updates' hidden={!ballotUpdatesConditionsMet}
-                                    disabled={election.state !== 'draft' || ballotUpdatesDisabled} checked={ballotUpdates} onChange={onChangeBallotUpdates} helperText={ballotUpdatesDisabledMsg}/>}
-                                { ['draft', 'finalized', 'open'].includes(election.state) && <CheckboxSetting setting='public_results' checked={publicResults}
-                                    onChange={onChangePublicResults} disabled={election.state !== 'draft' || publicResultsDisabled} helperText={publicResultsDisabled}/>}
-                                <CheckboxSetting setting='require_instruction_confirmation'/>
-                                <CheckboxSetting setting='draggable_ballot'/>
-                                <CheckboxSetting setting='is_public'/>
-                                <CheckboxSetting setting='max_rankings' onChange={(e) => applySettingsUpdate(settings => {
-                                    settings.max_rankings = e.target.checked ? default_rankings : undefined })
-                                }/>
-
-                                <TextField
-                                    id="rank-limit"
-                                    type="number"
-                                    value={editedElectionSettings.max_rankings ? editedElectionSettings.max_rankings : default_rankings}
-                                    onChange={(e) => applySettingsUpdate((settings) => { settings.max_rankings = Number(e.target.value) })}
-                                    variant='standard'
-                                    InputProps={{ inputProps: { min: min_rankings, max: max_rankings, "aria-label": "Rank Limit" } }}
-                                    sx={{ pl: 4, mt: -1, display: 'block'}}
-                                    disabled={election.state !== 'draft' || !editedElectionSettings.max_rankings}
-                                />
-
-                                
-                            </FormGroup>
-                        </FormControl>
-                    </Grid >
-                </DialogContent>
-                <DialogActions>
-                    { election.state === 'draft' ?
-                        <>
-                            <SecondaryButton
-                                type='button'
-                                fullWidth={false}
-                                onClick={handleClose}
-                            >
-                                {t('keyword.cancel')}
-                            </SecondaryButton>
-                            <PrimaryButton
-                                type='button'
-                                variant='contained'
-                                fullWidth={false}
-                                onClick={() => onSave()}
-                            >
-                                {t('keyword.save')}
-                            </PrimaryButton>
-                        </>
-                    :
-                        <PrimaryButton
-                            type='button'
-                            variant='contained'
-                            fullWidth={false}
-                            onClick={handleClose}
-                        >
-                            {t('keyword.close')}
-                        </PrimaryButton>
-                    }
-                </DialogActions>
-            </Dialog>
-        </Paper>
-    )
+                <TextField
+                    id="rank-limit"
+                    type="number"
+                    value={editedElectionSettings.max_rankings ? editedElectionSettings.max_rankings : default_rankings}
+                    onChange={(e) => applySettingsUpdate((settings) => { settings.max_rankings = Number(e.target.value) })}
+                    variant='standard'
+                    InputProps={{ inputProps: { min: min_rankings, max: max_rankings, "aria-label": "Rank Limit" } }}
+                    sx={{ pl: 4, mt: -1, display: 'block'}}
+                    disabled={election.state !== 'draft' || !editedElectionSettings.max_rankings}
+                />
+            </FormGroup>
+        </FormControl>
+    </Grid >
 }
