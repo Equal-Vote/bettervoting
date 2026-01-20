e6f70b9c5a250043b730d381e1bf1e54cd3a0d91
Arend Peter <apc1993@gmail.com>
2026-01-19 23:58:27 -0800

    Fix broken test

--- packages/backend/src/Services/azureBlobService.ts
+++ packages/backend/src/Services/azureBlobService.ts
@@ -1,6 +1,6 @@
 import { BlobServiceClient } from "@azure/storage-blob";
 
-const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING;
+const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING || '';
 if (!connectionString) {
   throw new Error('AZURE_STORAGE_CONNECTION_STRING is not set');
 }
