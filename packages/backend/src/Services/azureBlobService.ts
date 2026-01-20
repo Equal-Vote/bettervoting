bf03e9628dffbc2acd18d4960fbc22372f55cd64
Arend Peter <apc1993@gmail.com>
2026-01-19 23:59:43 -0800

    Remove impossible error

--- packages/backend/src/Services/azureBlobService.ts
+++ packages/backend/src/Services/azureBlobService.ts
@@ -1,9 +1,6 @@
 import { BlobServiceClient } from "@azure/storage-blob";
 
 const connectionString = process.env.AZURE_STORAGE_CONNECTION_STRING || '';
-if (!connectionString) {
-  throw new Error('AZURE_STORAGE_CONNECTION_STRING is not set');
-}
 
 const blobServiceClient = BlobServiceClient.fromConnectionString(connectionString);
 
