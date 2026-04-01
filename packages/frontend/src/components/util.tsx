b948dde62d402689af2b943068e4e3d73e7bf736
Arend Peter <apc1993@gmail.com>
2026-03-31 22:14:27 -0700

    Fix styling on switch buttons

--- packages/frontend/src/components/util.tsx
+++ packages/frontend/src/components/util.tsx
@@ -414,6 +414,7 @@ export function SwitchSetting({ label, toggled, onToggle, disabled, disabledMess
               color: '#fff',
               '&.Mui-checked': {
                 transform: 'translateX(16px)',
+                color: '#fff',
                 '& + .MuiSwitch-track': { opacity: 1 },
               },
               '&.Mui-disabled + .MuiSwitch-track': { opacity: 0.5 },
