9db4e063b778309ab49e33ec406b16f8abb15b3c
Arend Peter <apc1993@gmail.com>
2026-05-13 10:43:48 -0700

    Initial AI pass

--- packages/shared/src/domain_model/ElectionSettings.ts
+++ packages/shared/src/domain_model/ElectionSettings.ts
@@ -73,9 +73,6 @@ function authenticationValidation(obj:authentication): string | null {
 function settingsCompatiblityValidation(settings: ElectionSettings, electionState?: ElectionState): string {
     let errorMsg = ''
     if (settings.ballot_updates) {
-        if (settings.public_results && !['closed', 'archived'].includes(electionState ?? '')) {
-            errorMsg += 'Preliminary results are not permitted when ballot updating is enabled.  ';
-        }
         if (settings.voter_access == 'open') {
             errorMsg += 'Ballot updating is not permitted on open access elections.  ';
         }
